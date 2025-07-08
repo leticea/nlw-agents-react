ecab22fc6c8a0bce7be109b32dce7b18720f464b
leticea <nascimento.leticeah@gmail.com>
2025-07-08 15:42:11 -0300

    feat: creating routes

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,14 @@
-import { Button } from "./components/ui/button";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CreateRoom } from "./pages/create-room";
+import { Room } from "./pages/room";
 
 export function App() {
   return (
-    <div>
-      <Button>Hello world</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<CreateRoom />} index />
+        <Route path="/room" element={<Room />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
