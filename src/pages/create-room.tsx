43360b22c54192bfc9f6136698f7cce006803bac
leticea <nascimento.leticeah@gmail.com>
2025-07-09 15:57:40 -0300

    feat: fetching data from API

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,9 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 
+type GetRoomsAPIResponse = Array<{
+  id: string;
+  name: string;
+}>;
+
 export function CreateRoom() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["get-rooms"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:3333/rooms");
+      const result: GetRoomsAPIResponse = await response.json();
+
+      return result;
+    },
+  });
+
   return (
     <div>
       <div>Create Room</div>
+
+      {isLoading && <p>Carregando...</p>}
+
       <Link className="underline" to="/room">
         Acessar sala
       </Link>
