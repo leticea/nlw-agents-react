344608dd8799bd4210e2153d7e5465edf601012d
leticea <nascimento.leticeah@gmail.com>
2025-07-10 21:28:13 -0300

    feat: create room response, create room request

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -3,11 +3,9 @@ import { RoomList } from "@/components/room-list";
 
 export function CreateRoom() {
   return (
-    <div className="min-h-screen py-8 px-4">
+    <div className="min-h-screen px-4 py-8">
       <div className="mx-auto max-w-4xl">
-        <div className="grid gap-8 grid-cols-2 items-start">
-          <div />
-
+        <div className="grid grid-cols-2 items-start gap-8">
           <CreateRoomForm />
           <RoomList />
         </div>
