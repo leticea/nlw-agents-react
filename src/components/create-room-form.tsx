1db3542b0b2e2a5b6bec6dae922b2c20db6a279c
leticea <nascimento.leticeah@gmail.com>
2025-07-23 12:12:57 -0300

    feat: creating mutate function

--- src/components/create-room-form.tsx
+++ src/components/create-room-form.tsx
@@ -19,15 +19,18 @@ import {
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
+import { useCreateRoom } from "@/http/use-create-room";
 
 const createRoomSchema = z.object({
   name: z.string().min(3, { message: "Inclua no mínimo 3 caracteres" }),
-  description: z.string().optional(),
+  description: z.string(),
 });
 
 type CreateRoomFormData = z.infer<typeof createRoomSchema>;
 
 export function CreateRoomForm() {
+  const { mutateAsync: createRoom } = useCreateRoom();
+
   const createRoomForm = useForm<CreateRoomFormData>({
     resolver: zodResolver(createRoomSchema),
     defaultValues: {
@@ -36,8 +39,8 @@ export function CreateRoomForm() {
     },
   });
 
-  function handleCreateRoom(data: CreateRoomFormData) {
-    console.log(data);
+  async function handleCreateRoom({ name, description }: CreateRoomFormData) {
+    await createRoom({ name, description });
   }
 
   return (
